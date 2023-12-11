f64569f88259ed566253f7b4a3095cfc8f192246
rpineda26 <91407317+rpineda26@users.noreply.github.com>
2023-12-12 05:35:56 +0800

    add: monokai color scheme definitions in css

--- server/routes/main.js
+++ server/routes/main.js
@@ -7,8 +7,8 @@ router.get('/', function(req,res){res.render('layouts/index')});
 router.get('/getHeroes', heroController.getHeroesList);
 router.get('/hero', heroController.getHeroesList);
 router.get('/hero/:hero_name', heroController.getHeroPage);
-router.get('/item', itemController.getItemsList);
-router.get('/item/:item_name', itemController.getItemPage); 
+//router.get('/item', itemController.getItemsList);
+//router.get('/item/:item_name', itemController.getItemPage); 
 
 
 module.exports = router;
