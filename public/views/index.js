92d5e1c36e1b805acd2c962dc74a69483c21a01b
rpineda26 <91407317+rpineda26@users.noreply.github.com>
2023-12-15 04:43:37 +0800

    fix: double scrollbar

--- public/views/index.js
+++ public/views/index.js
@@ -1,18 +1,35 @@
 
 const sidebar = document.querySelector('#side-bar');
 const section = document.getElementsByClassName('section');
+const sidebar_links = document.querySelectorAll('#side-bar a');
 console.log(section);
 console.log(sidebar);
-console.log(window.innerHeight);
 console.log(section[0].textContent);
+console.log(sidebar_links);
+
+sidebar.addEventListener("change", function(e){
+	console.log('99');
+	const true_focus = false;
+	for(let i=0;i<section.length-1;i++){
+		console.log(sidebar_links[i].classList);
+		if(sidebar_links[i].classList.contains('active'))
+			true_focus = true;
+	}
+	if(!true_focus)
+		sidebar.style.display = 'none';
+	else
+		sidebar.style.display = 'block';
+});
+
 section[0].addEventListener('click',()=>{
-	window.onscroll= null;
 	console.log('click'+section[0].textContent);
 	section[0].scrollIntoView({behavior:'smooth'});
 });
 section[1].addEventListener('click',()=>{
 	console.log('click');
+	
 	section[1].scrollIntoView({behavior:'smooth'});
+
 });
 
 section[2].addEventListener('click',()=>{
