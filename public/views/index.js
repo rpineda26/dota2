747185a78e725c3c26d92193f4c15165ab11824e
rpineda26 <91407317+rpineda26@users.noreply.github.com>
2023-12-15 17:13:44 +0800

    fix: sidebar sticky

--- public/views/index.js
+++ public/views/index.js
@@ -2,25 +2,6 @@
 const sidebar = document.querySelector('#side-bar');
 const section = document.getElementsByClassName('section');
 const sidebar_links = document.querySelectorAll('#side-bar a');
-sidebar.classList.add('d-none');
-document.addEventListener("wheel", function(e){
-	console.log('99');
-	let true_focus = false;
-	for(let i=0;i<section.length-1;i++){
-		console.log(sidebar_links[i].classList);
-		if(sidebar_links[i].classList.contains('active'))
-			true_focus = true;
-	}
-	console.log(true_focus);
-	if(!true_focus){
-		sidebar.classList.remove('active');
-		sidebar.classList.add('d-none');
-	}
-	else{
-		sidebar.classList.add('active');
-		sidebar.classList.remove('d-none');
-	}
-});
 
 section[0].addEventListener('click',()=>{
 	console.log('click'+section[0].textContent);
