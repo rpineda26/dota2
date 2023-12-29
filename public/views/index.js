d499fcc1943ab7352001be3938039de03767e24e
rpineda26 <91407317+rpineda26@users.noreply.github.com>
2023-12-30 04:48:35 +0800

    add: gsap library, flare on mouse move in first page and scroll animation for the down buttons

--- public/views/index.js
+++ public/views/index.js
@@ -1,10 +1,10 @@
-
+gsap.registerPlugin(ScrollTrigger);
 const sidebar = document.querySelector('#side-bar');
 const section = document.getElementsByClassName('section');
 const sidebar_links = document.querySelectorAll('#side-bar a');
 const downArrow = document.querySelectorAll('.bi.bi-chevron-compact-down');
 const backTop = document.querySelector('#backtotop');
-
+console.log(section);
 console.log(downArrow);
 document.body.addEventListener('scroll',()=>{
 	console.log('scroll');
@@ -12,14 +12,39 @@ document.body.addEventListener('scroll',()=>{
 
 
 for(let i=0;i<downArrow.length;i++){
+	console.log(section[i]);
 	downArrow[i].addEventListener('click',()=>{
-		scrollHere(i+1);
+		section[i+1].scrollIntoView({behavior:'smooth', block:'center'});
 	});
 }
 backTop.addEventListener('click',()=>{
-	scrollHere(0);
+	section[0].scrollIntoView({behavior:'smooth'});
 });
-function scrollHere(index){
-	console.log(`scroll to section ${index}`);
-	section[index].scrollIntoView({behavior:'smooth'});
+
+for(let i=0;i<downArrow.length;i++){
+	gsap.to(downArrow[i],{
+		scrollTrigger: {
+			trigger: downArrow[i],
+			start: "top bottom",
+			end: "bottom center",
+			toggleActions: "restart none restart none",
+			markers: true,
+		},
+		duration:0.5,
+		scale:2,
+	});
 }
+
+
+
+section[0].addEventListener("mousemove", function(e) {
+  gsap.to('.cursor', {transform:'translate(-50%,-50% scale(1)' ,left:e.x, top:e.y, duration: 0.6, ease: "power3"});
+});
+section[0].addEventListener('mouseenter',function(){
+	gsap.to('.cursor', {transform:'translate(-50%,-50%) scale(1)'});
+	console.log("mouseenter");
+});
+section[0].addEventListener('mouseleave',function(){
+	console.log("mouseleave");
+	gsap.to('.cursor', {transform:'translate(-50%,-50%) scale(0)'});
+});
